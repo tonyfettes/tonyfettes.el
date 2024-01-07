ce059c23d83f6a4b833d0aa6bc53d7f6d6b6ee10
Haoxiang Fei <haoxiang@Haoxiangs-MacBook-Pro.local>
2024-01-07 17:43:57 +0800

    fix(org): don't insert space between chinese chars

--- init.el
+++ init.el
@@ -150,7 +150,19 @@
   (add-hook 'org-babel-after-execute-hook 'org-display-inline-images)
 
   ;; Render checkbox when export to HTML
-  (setq org-html-checkbox-type 'html))
+  (setq org-html-checkbox-type 'html)
+
+  (defadvice org-html-paragraph (before org-html-paragraph-advice
+                                        (paragraph contents info) activate)
+    "Join consecutive Chinese lines into a single long line without
+unwanted space when exporting org-mode to html."
+    (let* ((origin-contents (ad-get-arg 1))
+           (fix-regexp "[[:multibyte:]]")
+           (fixed-contents
+            (replace-regexp-in-string
+             (concat
+              "\\(" fix-regexp "\\) *\n *\\(" fix-regexp "\\)") "\\1\\2" origin-contents)))
+      (ad-set-arg 1 fixed-contents))))
 
 (use-package ox-latex
   :straight (:type built-in)
