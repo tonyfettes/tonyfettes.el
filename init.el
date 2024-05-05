dbcb75bcfa9cad82cfc0409d42a559fd41b9662b
Tony Fettes <tonyfettes@tonyfettes.com>
2024-05-06 00:41:25 +0800

    feat: org-roam

--- init.el
+++ init.el
@@ -169,6 +169,22 @@ unwanted space when exporting org-mode to html."
 
   (advice-add 'org-html-paragraph :before #'org-html-paragraph-join-chinese))
 
+;; Org-roam
+(use-package org-roam
+  :custom
+  (org-roam-directory (file-truename "~/documents/org/"))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  (require 'org-roam-protocol))
+
 (use-package ox-latex
   :ensure nil
   :config
